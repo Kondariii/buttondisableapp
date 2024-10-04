94a3fe9b512636b8a72608e212880bbf1482dd1e
broski <jakubkrzeminski08@gmail.com>
2024-10-04 23:16:43 +0200

    m

--- app/src/main/java/com/example/buttondisableapp/MainActivity.java
+++ app/src/main/java/com/example/buttondisableapp/MainActivity.java
@@ -1,7 +1,6 @@
 package com.example.buttondisableapp;
 
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
